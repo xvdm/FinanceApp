c191c242f1dfef517be517a64d77ffd06a6dfa5a
bondar_m <bondar_m@26-7>
2021-12-06 14:39:40 +0200

    All

--- FinanceExam/MainWindow.xaml.cs
+++ FinanceExam/MainWindow.xaml.cs
@@ -38,5 +38,33 @@ namespace FinanceExam
         {
             this.WindowState = WindowState.Minimized;
         }
+
+        private void grid_Loaded(object sender, RoutedEventArgs e)
+        {
+            
+            List<History> result = new List<History>();
+            result.Add(new History("06.12.2021", 600, "Thriller", ""));
+            result.Add(new History("06.12.2021", 700, "Back in Black", ""));
+            result.Add(new History("06.12.2021", -10, "Saturday Night Fever", ""));
+            result.Add(new History("06.12.2021", 30, "The Dark Side of the Moon", ""));
+            grid.ItemsSource = result;
+        }
     }
+
+    public class History
+    {
+        public History(string Id, double Vocalist, string Album, string Year)
+        {
+            this.Day = Id;
+            this.Money = Vocalist;
+            this.Category = Album;
+            this.Comment = Year;
+        }
+
+        public string Day { get; set; }
+        public double Money { get; set; }
+        public string Category { get; set; }
+        public string Comment { get; set; }
+    }
+
 }
