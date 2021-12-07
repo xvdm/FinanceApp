570919873ffbd73aa80bfe569835ece1fe9b307b
dsapuohgasd <75278833+dsapuohgasd@users.noreply.github.com>
2021-12-07 13:18:08 +0200

    Saves2

--- FinanceExam/MainWindow.xaml.cs
+++ FinanceExam/MainWindow.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -18,10 +20,13 @@ namespace FinanceExam
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
+
+    [Serializable]
     public partial class MainWindow : Window
     {
         List<History_Data> data_grid = null;
-        TransverSetting Transver = new TransverSetting();
+        [NonSerialized] Data data;
+        [NonSerialized] TransverSetting Transver = new TransverSetting();
         public MainWindow()
         {
             InitializeComponent();
@@ -29,6 +34,14 @@ namespace FinanceExam
         private void Window_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
         {
             this.DragMove();
+        }
+
+        public string GetUserSelectedCardName
+        {
+            get
+            {
+                return CardName.Text;
+            }
         }
 
         private void Button_Click_Exit(object sender, RoutedEventArgs e)
@@ -49,46 +62,50 @@ namespace FinanceExam
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            if(data_grid == null)
+            if (data_grid == null)
             {
                 data_grid = new List<History_Data>();
                 Datagrid.ItemsSource = data_grid;
             }
             data_grid.Add(new History_Data("06.12.2021", 600, "Вадим", "Диме"));
 
-
-
             Transver.General_Balance += 600;
             GeneralBalance.Content = Transver.General_Balance;
-
+           
             Datagrid.Items.Refresh();
+
+            data = new Data();
+            data.Save(data_grid,GetUserSelectedCardName);
         }
+
+       
 
         private void Сurrency_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-
-            if(Currency.SelectedItem .ToString() == "$")
+            if (Currency.SelectedItem.ToString() == "$")
             {
                 double temp = 0;
                 temp = Transver.General_Balance / 27.37;
                 GeneralBalance.Content = temp;
             }
         }
+
+        private void CardName_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            data_grid = data.Load(GetUserSelectedCardName);
+            Datagrid.Items.Refresh();
+        }
     }
 
     public class TransverSetting
     {
         private double MONEY = 0;
-
-
         public double General_Balance { get { return MONEY; } set { MONEY = value; } }
-
-
     }
 
+    [Serializable]
     public class History_Data
     {
-
         private string day;
         private double money;
         private string category;
@@ -101,13 +118,47 @@ namespace FinanceExam
             this.category = _category;
             this.comment = _comment;
         }
-
         public string Day { get { return this.day; } set { this.day = value; } }
         public double Money { get { return this.money; } set { this.money = value; } }
         public string Category { get { return this.category; } set { this.category = value; } }
         public string Comment { get { return this.comment; } set { this.comment = value; } }
-
-
-
     }
+
+    public class Data
+    {
+        private string path = @"C:\FinanceData";
+        private BinaryFormatter formatter;
+        public void Save(List<History_Data> data,string name)
+        {
+            CreateDataFile();
+            formatter = new BinaryFormatter();
+            using (FileStream fs = new FileStream(path + '\\' + name +".txt", FileMode.OpenOrCreate))
+            {
+                formatter.Serialize(fs, data);
+            }
+        }
+
+        public List<History_Data> Load( string name)
+        {
+            CreateDataFile();
+            formatter = new BinaryFormatter();
+            using (FileStream fs = new FileStream(path+ '\\' + name + ".txt", FileMode.OpenOrCreate))
+            {
+                return (List<History_Data>)formatter.Deserialize(fs);
+            }
+        }
+
+        private void CreateDataFile()
+        {
+            DirectoryInfo dirInfo = new DirectoryInfo(path);
+            if (!dirInfo.Exists)
+            {
+                dirInfo.Create();
+            }
+        }
+    }
+
+
+
+
 }
--- FinanceExam/User.cs
+++ FinanceExam/User.cs
@@ -8,46 +8,46 @@ using System.Windows;
 
 namespace FinanceExam
 {
-    class Card
-    {
-        private string card;
-        private string path = @"C:\FinanceData";
+    //class Card
+    //{
+    //    private string card;
+    //    private string path = @"C:\FinanceData";
 
-        public Card()
-        {
-            DirectoryInfo dirInfo = new DirectoryInfo(path);
-            if (!dirInfo.Exists)
-            {
-                dirInfo.Create();
-            }
-        }
+    //    public Card()
+    //    {
+    //        DirectoryInfo dirInfo = new DirectoryInfo(path);
+    //        if (!dirInfo.Exists)
+    //        {
+    //            dirInfo.Create();
+    //        }
+    //    }
 
-        public void AddCard()
-        {
-            card = "Карта Вадима1";
-            string userpath = path + '\\' + card + ".txt";
-            if (!File.Exists(userpath))
-            {
-                File.Create(userpath);
-            }
-            else
-            {
-                MessageBox.Show("Такая карта уже существует!", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Information);
-            }
-        }
-        public void SwitchUserCard()
-        {
+    //    public void AddCard()
+    //    {
+    //        card = "Карта Вадима1";
+    //        string userpath = path + '\\' + card + ".txt";
+    //        if (!File.Exists(userpath))
+    //        {
+    //            File.Create(userpath);
+    //        }
+    //        else
+    //        {
+    //            MessageBox.Show("Такая карта уже существует!", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Information);
+    //        }
+    //    }
+    //    public void SwitchUserCard()
+    //    {
 
-        }
+    //    }
         
-        public void Save()
-        {
+    //    public void Save()
+    //    {
             
-        }
+    //    }
 
-        public void Load()
-        {
+    //    public void Load()
+    //    {
 
-        }
-    }
+    //    }
+    //}
 }
