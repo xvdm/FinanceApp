3cbf44587ea34af7f64367d28ec73d217b52f5d5
dsapuohgasd <75278833+dsapuohgasd@users.noreply.github.com>
2021-12-07 13:17:53 +0200

    Saving

---
+++ FinanceExam/User.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Windows;
+
+namespace FinanceExam
+{
+    class Card
+    {
+        private string card;
+        private string path = @"C:\FinanceData";
+
+        public Card()
+        {
+            DirectoryInfo dirInfo = new DirectoryInfo(path);
+            if (!dirInfo.Exists)
+            {
+                dirInfo.Create();
+            }
+        }
+
+        public void AddCard()
+        {
+            card = "Карта Вадима1";
+            string userpath = path + '\\' + card + ".txt";
+            if (!File.Exists(userpath))
+            {
+                File.Create(userpath);
+            }
+            else
+            {
+                MessageBox.Show("Такая карта уже существует!", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+        }
+        public void SwitchUserCard()
+        {
+
+        }
+        
+        public void Save()
+        {
+            
+        }
+
+        public void Load()
+        {
+
+        }
+    }
+}
